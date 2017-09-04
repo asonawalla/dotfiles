9a9b72d7998c3c8d3cdfe2941de8372016186f37
Azim Sonawalla <azim.sonawalla@liveramp.com>
2017-09-04 16:20:58 -0700

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,24 @@
-# pippio go setup
+# go environment setup
 set -x GOROOT /usr/local/go
 set -x GOPATH $HOME/workspace
 set -x PATH $PATH $GOROOT/bin $GOPATH/bin
 
-# add gcloud sdk stuff to the path
+# add google cloud sdk bin to the path
 set -x PATH $PATH $HOME/google-cloud-sdk/bin
 
-# Testing keys for pippio-testing s3 bucket
-set -x AWS_ACCESS_KEY_ID ***REMOVED***
-set -x AWS_SECRET_ACCESS_KEY ***REMOVED***
+# use gnu coreutils by default (os x  only)
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 
-# legacy pippio stuff
-set -x PIPPIO_DEV 1
-set -x PIPPIO_ASSET_DIRECTORY $HOME/workspace/src/assets/
+# additional go build flags for mac os x. this works with the
+# brew install'd version of rocksdb. presumably.
+# set -x CGO_CPPFLAGS "-I/usr/local/include"
+# set -x CGO_LDFLAGS "-L'/usr/local/lib'"
 
-# android studio
-set -x ANDROID_STUDIO /opt/android-studio
-set -x PATH $PATH $ANDROID_PATH/bin
+# these cgo flags point rocksdb to the local installation.
+set rdb '/Users/asonaw/workspace/src/github.com/facebook/rocksdb'
+set -x CGO_CFLAGS '-I'$rdb'/include'
+set -x CGO_LDFLAGS '-L'$rdb' -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy'
+set -x CPATH $rdb'/include'
 
 # easy way to update submodules
 alias gsync='git submodule sync; and git submodule update --init --recursive'
@@ -27,7 +29,10 @@ alias production='kubectl config use-context production'
 alias context='kubectl config current-context'
 alias cycle-postgres='kubectl get pod -l app=postgresql -o json | jq -r .items[0].metadata.name | xargs -n 1 kubectl delete pod'
 
-# node version manager
-# set -x NVM_DIR "/home/azim/.nvm"
-# bass source "$NVM_DIR/nvm.sh"  # This loads nvm
+# for some reason thefuck isnt working in functions and im too lazy to debug why
+# eval (thefuck --alias | tr '\n' ';')
 
+set -g fish_user_paths "/usr/local/opt/bison@2.7/bin" $fish_user_paths
+
+# vg auto setup
+command -v vg >/dev/null 2>&1; and vg eval --shell fish | source
