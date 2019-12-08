70e66d0f791c25dbcc7aeb34554b63c41d28b081
Azim Sonawalla <azim.sonawalla@gmail.com>
2019-12-08 22:26:58 +0000

    Install microk8s by default

--- mk/build.mk
+++ mk/build.mk
@@ -1,6 +1,6 @@
 #!/usr/bin/env make -f
 
-build: /snap/bin/go /usr/bin/docker
+build: /snap/bin/go /usr/bin/docker /snap/bin/microk8s.status
 
 /snap/bin/go:
 	sudo snap install go --classic
@@ -9,3 +9,7 @@ build: /snap/bin/go /usr/bin/docker
 /usr/bin/docker:
 	sudo apt-get install --yes docker.io
 	sudo usermod -aG docker $(USER)
+
+/snap/bin/microk8s.status:
+	sudo snap install microk8s --classic
+	sudo usermod -aG microk8s $(USER)
