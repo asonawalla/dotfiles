990e55d3a00de1ff7a07b6bcb9df5eca0e19d0a0
Azim Sonawalla <azim.sonawalla@gmail.com>
2019-12-08 18:18:31 -0500

    Use snap provideded docker

--- mk/build.mk
+++ mk/build.mk
@@ -1,14 +1,15 @@
 #!/usr/bin/env make -f
 
-build: /snap/bin/go /usr/bin/docker /snap/bin/microk8s.status
+build: /snap/bin/go /snap/bin/docker /snap/bin/microk8s.status
 
 /snap/bin/go:
 	sudo snap install go --classic
 	vim +GoInstallBinaries +qall
 
-/usr/bin/docker:
-	sudo apt-get install --yes docker.io
-	sudo usermod -aG docker $(USER)
+/snap/bin/docker:
+	sudo addgroup --system docker
+	sudo adduser $(USER) docker
+	echo sudo snap install docker | newgrp docker
 
 /snap/bin/microk8s.status:
 	sudo snap install microk8s --classic
